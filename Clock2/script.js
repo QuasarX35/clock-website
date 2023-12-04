d2d3ff36a3cfddfd792908103ea4aece1113acdc
QuasarX35 <shootingquasar555@gmail.com>
2023-12-04 23:26:10 +0800

    changed measurements to percentage and added weather

--- Clock2/script.js
+++ Clock2/script.js
@@ -17,15 +17,32 @@ function updateClock() {
     const hourDeg = (hours / 12) * 360 + (minutes / 60) * 30;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
 
-    document.getElementById('time').textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds}`;
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+
+    document.getElementById('date').textContent = `${month + 1}/${day}/${year}`;
+    document.getElementById('time').textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
 function updateWeather() {
-    // Simulated weather data
-    const weatherData = {
-        temperature: 25,
-        condition: 'Sunny'
-    };
+
+    // API Key methods
+    var apiKey = '12345';
+    Weather.setApiKey(apiKey);
+    var tempApiKey = Weather.getApiKey();
+
+    const weatherData = null;
+
+    // Get the current weather for a given city using the latitude and longitude
+    var lat = 3.136899260164333,
+        long = 101.73367344637204;
+    Weather.getCurrentByLatLong(lat, long, function (current) {
+        weatherData = {
+            temperature: current.temperature(),
+            condition: current.conditions()
+        };
+    });
 
     document.getElementById('weather').textContent = `Weather: ${weatherData.condition}, ${weatherData.temperature}°C`;
 }
