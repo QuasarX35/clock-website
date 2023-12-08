87e8eca2e26253258e3df951ad6e4f3b3cd9a98e
QuasarX35 <shootingquasar555@gmail.com>
2023-12-08 12:29:40 +0800

    added braun and lemnos clock skins

--- script.js
+++ script.js
@@ -1,28 +1,85 @@
-const secondHand = document.getElementById('secHand');
-const minHand = document.getElementById('minHand');
-const hourHand = document.getElementById('hourHand');
+const clock = document.querySelector('.clock');
+const braunClock = document.querySelector('.braunClock');
+const lemnosClock = document.querySelector('.lemnosClock');
+const clocks = [braunClock, lemnosClock];
+
+function showBraunClock() {
+    clocks.forEach(element => {
+        element.style.display = 'none';
+    });
+    braunClock.style.display = 'block';
+}
+
+function showLemnosClock() {
+    clocks.forEach(element => {
+        element.style.display = 'none';
+    });
+    lemnosClock.style.display = 'block';
+}
+
+function drawMarks() {
+    // braun clock
+    const braunMarks = braunClock.getElementsByClassName('mark');
+    for (var i = 0; i < 60; i++) {
+        braunClock.innerHTML += "<div class='mark'></div>";
+        braunMarks[i].style.transform = `rotate(${6 * i}deg)`;
+    }
+
+    // lemnos clock
+    const lemnosMarks = lemnosClock.getElementsByClassName('mark');
+    for (var i = 0; i < 60; i++) {
+        lemnosClock.innerHTML += "<div class='mark'></div>";
+        lemnosMarks[i].style.transform = `rotate(${6 * i}deg)`;
+    }
+}
+
+function drawNumbers() {
+    // braun clock
+    for (var i = 1; i <= 12; i++) {
+        const hourNumber = document.createElement('span');
+        hourNumber.classList.add('hourNumber');
+        hourNumber.textContent = i;
+        hourNumber.style.transform = `rotateZ(calc((${i} + 6) * calc(360/12) * 1deg)) translateY(410%) rotateZ(calc((${i} + 6) * calc(360/12) * -1deg))`;
+        braunClock.appendChild(hourNumber);
+    }
+}
+
 
 function updateClock() {
+    const secondHand = document.querySelectorAll("[id='secHand']");
+    const minHand = document.querySelectorAll("[id='minHand']");
+    const hourHand = document.querySelectorAll("[id='hourHand']");
+
     const now = new Date();
 
     const seconds = now.getSeconds();
     const secondDeg = (seconds / 60) * 360;
-    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    secondHand.forEach(element => {
+        element.style.transform = `rotate(${secondDeg}deg)`;
+    });
 
     const minutes = now.getMinutes();
     const minuteDeg = (minutes / 60) * 360 + (seconds / 60) * 6;
-    minHand.style.transform = `rotate(${minuteDeg}deg)`;
+    minHand.forEach(element => {
+        if (!(element.parentNode == lemnosClock)) {
+            element.style.transform = `rotate(${minuteDeg}deg)`;
+        } else {
+            document.getElementById('rectangle').style.transform = `rotate(${minuteDeg}deg)`;
+        }
+    });
 
     const hours = now.getHours();
     const hourDeg = (hours / 12) * 360 + (minutes / 60) * 30;
-    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    hourHand.forEach(element => {
+        element.style.transform = `rotate(${hourDeg}deg)`;
+    });
 
     const day = now.getDate();
     const month = now.getMonth();
     const year = now.getFullYear();
 
-    document.getElementById('date').textContent = `${month + 1}/${day}/${year}`;
-    document.getElementById('time').textContent = `${hours == 0 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    document.getElementById('date').textContent = `${day}/${month + 1}/${year}`;
+    document.getElementById('time').textContent = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
 const apiKey = "b0855f0d337eb10cea885183cb8317e4";
@@ -32,7 +89,6 @@ async function checkWeather() {
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
 
-    console.log(data);
     document.getElementById('cityNameAndTemp').textContent = `${data.name}, ${data.main.temp.toFixed(1)}°C`;
     document.getElementById('weather').textContent = `${data.weather[0].main}, ${data.weather[0].description}`;
     document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;
@@ -44,5 +100,7 @@ function update() {
     checkWeather();
 }
 
+drawMarks();
+drawNumbers();
 setInterval(update, 1000);
 update(); // Initial update
